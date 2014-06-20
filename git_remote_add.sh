d39596b116836263e57c7a369788d80cb6a93296
aaron.gundel <aaron.gundel@gmail.com>
2014-06-19 18:44:12 -0700

    Added some comments to the git remote creation script

--- git_remote_add.sh
+++ git_remote_add.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+# This script initializes the git repository, creates the README, opens it for editing, 
+# allows for initial commit before prompting for the git repository (which you should have
+# already created) and pushing the repository to the new remote.  
+
 git init
 touch README.md
 echo "Opening README in vi for editing.  (i to edit; :q to exit)" 
